67bec7efc596c26ff3edad1a1752e55ee6c8f8c6
monthebrice2000 <65128522+monthebrice2000@users.noreply.github.com>
2022-10-23 14:42:36 +0200

    add another API rest for login and register and events api

--- index.js
+++ index.js
@@ -1,135 +1,314 @@
-//import  dotenv from "dotenv";
-//import  pg from "pg";
-import jquery from "jquery";
-
 import pg from "pg";
-
-/*let connectDb = async () => {
-    try {
-        const client = new pg.Client({
-            user: 'monthedjeumoubrice2000',
-            host: 'db.bit.io',
-            database: 'monthedjeumoubrice2000/Users',
-            password: 'v2_3uB8N_9kGEykC9hBtAF6hGFckCCp4',
-            port: '5432'
-        });
-
-        console.log( client );
-        await client.connect()
-        const res = await client.query('SELECT * FROM dept')
-        console.log(res)
-        await client.end()
-    } catch (error) {
-        console.log(error)
-    }
-}*/
-
-// Requiring Express from node modules
+import oidc from "express-openid-connect";
+const { auth, requiresAuth } = oidc;
 import express from "express";
-
-// Requiring Path from node modules
 import path from "path";
-
-// @ADD this, to require dotenv and configure it
+import cors from "cors";
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import cookierSession from "cookie-session";
+import passport from "passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import {
+  ReasonPhrases,
+  StatusCodes,
+  getReasonPhrase,
+  getStatusCode,
+} from "http-status-codes";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
-const dotenvConfig = dotenv.config();
+import fs from "fs";
+dotenv.config();
 
 // Creating express instance and save it to app variable
-const app = express()
+const app = express();
 let client = null;
 
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: "airforce#",
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (payload, done) => {
+      await connectDB().catch((error) => done(null, false));
+      await readDB(payload.subject.email, payload.subject.password, client)
+        .then((success) => {
+          done(null, {
+            email: payload.subject.email,
+            password: payload.subject.password,
+          });
+        })
+        .catch((error) => done(null, false));
+      await client.end();
+    }
+  )
+);
+
 // Setup the app to serve static files
-app.use(express.static(path.join('./public')))
-app.use( express.json({ extended: true, limit: '1mb' }));
+app.use(express.static(path.join("./public")));
+app.use(passport.initialize());
+app.set("views", path.join("./public/views"));
+app.set("view engine", "ejs");
+app.use(express.json({ extended: true, limit: "1mb" }));
+//add other middleware
+app.use(cors());
+app.use(
+  cookierSession({
+    httpOnly: true,
+    keys: "test",
+    name: "tokenn",
+    signed: true,
+    secret: "test",
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(morgan('dev'));
 
-app.post( "/registration", async (req, res) => {
-    //console.log( req.body )
-    const { username, email, password } = req.body;
-    //console.log( req.body )
-    await connectDB().catch( error => res.status(404).json( { username, email, password } )  );
-    //console.log( client )
-    await insertDB( username, email, password , client )
-        .then( success => res.status(200).json( { username, email, password } ) )
-        .catch( error => res.status(404).json( { username, email, password } ));
-    await client.end();
-})
+/*
+app.get("/callback", async (req, res, next) => {
+  if (req.query.code) next();
+  else res.status(302).redirect("/profile");
+});
 
-app.post( "/login", async (req, res) => {
-    //console.log( req.body )
-    const {  email, password } = req.body;
-    //console.log( req.body )
-    await connectDB().catch( error => res.status(404).json( { email, password } )  );
-    //console.log( client )
-    await readDB( email, password , client )
-        .then( success => res.status(200).json( {email, password } ) )
-        .catch( error => res.status(404).json( { email, password } ));
-    await client.end();
-})
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    secret: process.env.SECRET,
+    clientSecret: process.env.SECRET,
+    idpLogout: true,
+    routes: {
+      postLogoutRedirect: process.env.BASE_URL,
+    },
+    authorizationParams: {
+      response_type: "code",
+      scope: "openid email profile",
+      audience: process.env.AUDIENCE,
+      redirect_uri: process.env.REDIRECT_URI,
+    },
+    logoutParams: {},
+  })
+);
+
+app.get("/profile", requiresAuth(), async (req, res) => {
+  res.render("profile", { user: req.oidc.user });
+});
+
+app.get("/", async (req, res) => {
+  res.render("main", { user: req.oidc?.user });
+});
+*/
+
+//app.set('set',true)
+app.post("/registration", async (req, res) => {
+  //console.log( req.body )
+  const { email, password } = req.body;
+  //console.log(req.body);
+  await connectDB().catch((error) =>
+    res.status(404).json({ message: "Cannot connect application" })
+  );
+  //console.log( client )
+  await insertDB(email, password, client)
+    .then((success) => res.status(200).json({ email, password }))
+    .catch((error) =>
+      res.status(404).json({ message: error.message || "Cannot register user" })
+    );
+  await client.end();
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  //console.log( req.body )
+  await connectDB().catch((error) => res.status(404).json({ email, password }));
+  //console.log( client )
+  await readDB(email, password, client)
+    .then((success) => {
+      let payload = { subject: { email, password } };
+      let token = jwt.sign(payload, "airforce#");
+      res
+        .status(StatusCodes.OK)
+        .cookie("token", token, { httpOnly: true })
+        .json({
+          email,
+          password,
+          message: getReasonPhrase(StatusCodes.OK),
+          token,
+        });
+    })
+    .catch((error) =>
+      res.status(StatusCodes.UNAUTHORIZED).json({
+        message: error.message,
+        msg: getReasonPhrase(StatusCodes.UNAUTHORIZED),
+      })
+    );
+  await client.end();
+});
+
+app.get(
+  "/events",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    console.log(req.user);
+    if (!req.user) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: getReasonPhrase(StatusCodes.UNAUTHORIZED) });
+    }
+    const { email, password } = req.user;
+    axios
+      .get("https://zoo-animal-api.herokuapp.com/animals/rand/7", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        res.status(StatusCodes.OK).json({ data: response.data });
+      })
+      .catch((err) =>
+        res.status(StatusCodes.BAD_REQUEST).json({
+          msg: err.message,
+          message: getReasonPhrase(StatusCodes.BAD_REQUEST),
+        })
+      );
+  }
+);
+
+app.get(
+  "/special-events",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    console.log(req.user);
+    if (!req.user) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: getReasonPhrase(StatusCodes.UNAUTHORIZED) });
+    }
+    const { email, password } = req.user;
+    axios
+      .get("https://zoo-animal-api.herokuapp.com/animals/rand/5", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        res.status(StatusCodes.OK).json({ data: response.data });
+      })
+      .catch((err) =>
+        res.status(StatusCodes.BAD_REQUEST).json({
+          msg: err.message,
+          message: getReasonPhrase(StatusCodes.BAD_REQUEST),
+        })
+      );
+  }
+);
+
+app.get("/config/:file_name", async (req, res) => {
+  let file_name = null;
+  try {
+    file_name = req.params.file_name;
+
+    res.sendFile(file_name, { root: "./config" }, (err, test, data) => {
+      if (err) {
+        const e = new Error("no such file or directory,");
+        e.message = "no such file or directory,";
+        e.statusCode = 404;
+        res.send("File Not Found" || e.message);
+      }
+    });
+  } catch (e) {
+    res.json({
+      statusCode: 404 || e.statusCode,
+      message: "File Not Found" || e.message,
+    });
+  }
+});
+
+app.use((req, res, next) => {
+  res.render("404");
+});
 
 // Setup our express server with env port
-let port = process.env.PORT || 5000 ;
-app.listen(port, ()=>{
-    console.log(`App is running on port 5000 `);
-    //connectDB();
-})
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port} `);
+  //connectDB();
+});
 
-const connectDB = async (  ) => {
+const connectDB = async () => {
+  try {
+    client = new pg.Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+      ssl: process.env.PGSSL,
+      connectionTimeoutMillis: 0,
+    });
 
-    try {
-        client = new pg.Client( {
-            user: "monthedjeumoubrice2000",
-            host: "db.bit.io",
-            database: "monthedjeumoubrice2000/Users",
-            password: "v2_3uB8N_9kGEykC9hBtAF6hGFckCCp4",
-            port: "5432",
-            ssl: true,
-            connectionTimeoutMillis: 0,
-        } );
+    /*client = new pg.Client({
+      user: process.env.PGUSER2,
+      host: process.env.PGHOST2,
+      database: process.env.PGDATABASE2,
+      password: process.env.PGPASSWORD2,
+      port: process.env.PGPORT2,
+      connectionTimeoutMillis: 0,
+    });*/
 
-        await client.connect().then( data => console.log( "connect successfully")).catch( err => { throw new Error("Connection Error") } );
-    }catch( error ){
-        client = null;
-        console.log( "+++++++", error );
-        throw new Error( error.message || "Error Message");
+    await client
+      .connect()
+      .then((data) => console.log("connect successfully"))
+      .catch((err) => {
+        throw new Error("Connection Error");
+      });
+  } catch (error) {
+    client = null;
+    console.log("+++++++", error);
+    throw new Error(error.message || "Error Message");
+  }
+  return client;
+};
+
+const insertDB = async (email, password, client) => {
+  if (!client) return null;
+  try {
+    const res = await client.query(
+      `INSERT INTO dept(email, password) VALUES ( '${email}', '${password}' )`
+    );
+    //await client.end()
+    console.log(res.rows);
+    return res;
+  } catch (error) {
+    //console.log("++++++++++")
+    console.log(error);
+    throw new Error(error.message || "Error Message");
+  }
+};
+
+const readDB = async (email, password, client) => {
+  if (!client) return null;
+  try {
+    const res = await client.query(
+      `SELECT * FROM dept WHERE password='${password}' AND ( email='${email}' )`
+    );
+    //await client.end()
+    console.log(res.rows);
+    if (res.rows && res.rows.length !== 0) {
+      return res[0];
+    } else {
+      throw new Error("User Not Found");
     }
-    return client;
-}
-
-const insertDB = async ( username, email, password, client ) => {
-    if( !client )
-        return null;
-    try{
-        const res = await client.query(`INSERT INTO dept(name, email, password) VALUES ( '${username}', '${email}', '${password}' )`);
-        //await client.end()
-        console.log( res.rows );
-        return res;
-    }catch( error ){
-        //console.log("++++++++++")
-        console.log( error );
-        throw new Error("Error Message")
-    }
-
-}
-
-const readDB = async ( email, password, client ) => {
-    if( !client )
-        return null;
-    try{
-        const res = await client.query(`SELECT * FROM dept WHERE password='${password}' AND ( name='${email}' OR email='${email}' )`);
-        //await client.end()
-        console.log( res.rows );
-        if( res.rows && res.rows.length !== 0 ){
-            return res;
-        }else{
-            throw new Error("No Users");
-        }
-        return res;
-    }catch( error ){
-        //console.log("++++++++++")
-        console.log( error );
-        throw new Error( error.message || "Error Message")
-    }
-
-}
-
-
+    return res;
+  } catch (error) {
+    //console.log("++++++++++")
+    console.log(error);
+    throw new Error(error.message || "Error Message");
+  }
+};
--- public/js/test.js
+++ public/js/test.js
@@ -1,7 +1,80 @@
 
+let auth0 = null;
+const fetchAuthConfig = () => fetch("/config/auth_config.json");
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+
+    auth0 = await createAuth0Client({
+        domain: config.domain,
+        client_id: config.clientId
+    });
+};
+
+// NEW
+const updateUI = async () => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+const login = async (screen_hint) => {
+    /*await auth0.loginWithRedirect({
+        redirect_uri: "http://localhost:5002/callback", //"https://monthebrice2000.github.io/my-portfolio/",
+        //appState : window.location.origin,
+        scope: "openid",
+
+    });*/
+        window.location.href = "https://dev-nekgqc-w.us.auth0.com/authorize?response_type=code&client_id=fEsbnsodpluz0eOVyWhJ5Cbgg6Rwv3j1&redirect_uri=http://localhost:5002/callback&scope=openid%20name%20email%20profile";
+    //console.log( auth0 )
+};
+
+const logout = () => {
+    auth0.logout({
+        redirect_uri: "http://localhost:5002/callback"
+    });
+};
+
+
+
+window.onload = async () => {
+    await configureClient();
+
+    //updateUI();
+
+    const isAuthenticated = await auth0.isAuthenticated();
+    console.log( isAuthenticated )
+
+    if (isAuthenticated) {
+        // show the gated content
+        return;
+    }
+
+    // NEW - check for the code and state parameters
+    const query = window.location.search;
+    console.log( query )
+    console.log( query.includes("code=") , query.includes("state=") )
+    if (query.includes("code=") && query.includes("state=")) {
+
+        // Process the login state
+        await auth0.handleRedirectCallback();
+
+        //updateUI();
+
+        // Use replaceState to redirect the user away and remove the querystring parameters
+        window.history.replaceState({}, document.title, "/");
+    }
+}
+
+
+
+
 $("#signup-btn").on("click", function(e){
     e.preventDefault();
-    const formEl = $("#formAuthentication");
+    //logout();
+    //login("signup");
+    /*const formEl = $("#formAuthentication");
    if( formEl.is(":valid")  ){
        const username = $(formEl).find("#username").prop("value");
        const email = $(formEl).find("#email").prop("value");
@@ -11,6 +84,9 @@ $("#signup-btn").on("click", function(e){
        $.ajax({
            type: "POST",
            url: "/registration",
+           headers :{
+
+           },
            data: JSON.stringify( { username: username, email: email, password: password} ),
            contentType: "application/json;charset=utf-8",
            dataType:"json",
@@ -28,13 +104,15 @@ $("#signup-btn").on("click", function(e){
                }, 300)
            }
        })
-   }
+   }*/
 
 });
 
 $("#signin-btn").on("click", function(e){
     e.preventDefault();
-    const formEl = $("#formAuthentication");
+
+    login("login");
+   /* const formEl = $("#formAuthentication");
     if( formEl.is(":valid")  ){
         const email = $(formEl).find("#email").prop("value");
         const password = $(formEl).find("#password").prop("value");
@@ -42,7 +120,7 @@ $("#signin-btn").on("click", function(e){
         console.log( user );
         $.ajax({
             type: "POST",
-            url: "/login",
+            url: "/registration2",
             data: JSON.stringify( { email: email, password: password} ),
             contentType: "application/json;charset=utf-8",
             dataType:"json",
@@ -54,10 +132,54 @@ $("#signin-btn").on("click", function(e){
             error : ( error ) => {
                 $(document.body).html("<h1>FAIL LOGIN</h1>")
                 setTimeout(function(){
+                    //window.location.href="http://localhost:8080/realms/token_test/login-actions/registration?client_id=token_test_client";
                     window.location.reload();
                 }, 300)
             }
         })
-    }
+    }*/
 
 });
+
+$("#reset-btn").on("click", function(e){
+    e.preventDefault();
+    console.log("reset password");
+    //login("reset-password")
+    /*const resetObj = new Auth0ChangePassword({
+        container:         "change-password-widget-container",                // required
+        email:             "{{email | escape}}",                              // DO NOT CHANGE THIS
+        csrf_token:        "{{csrf_token}}",                                  // DO NOT CHANGE THIS
+        ticket:            "{{ticket}}",                                      // DO NOT CHANGE THIS
+        password_policy:   "{{password_policy}}",                             // DO NOT CHANGE THIS
+        password_complexity_options:  "{{password_complexity_options}}",        // DO NOT CHANGE THIS
+        theme: {
+            icon: "//cdn.auth0.com/styleguide/1.0.0/img/badge.png",
+            primaryColor: "{{tenant.colors.primary | default: '#ea5323'}}"
+        },
+        dict: {
+            // passwordPlaceholder: "your new password",
+            // passwordConfirmationPlaceholder: "confirm your new password",
+            // passwordConfirmationMatchError: "Please ensure the password and the confirmation are the same.",
+            // passwordStrength: {
+            //   containsAtLeast: "Contain at least %d of the following %d types of characters:",
+            //   identicalChars: "No more than %d identical characters in a row (e.g., "%s" not allowed)",
+            //   nonEmpty: "Non-empty password required",
+            //   numbers: "Numbers (i.e. 0-9)",
+            //   lengthAtLeast: "At least %d characters in length",
+            //   lowerCase: "Lower case letters (a-z)",
+            //   shouldContain: "Should contain:",
+            //   specialCharacters: "Special characters (e.g. !@#$%^&*)",
+            //   upperCase: "Upper case letters (A-Z)"
+            // },
+            // successMessage: "Your password has been reset successfully.",
+            // configurationError: "An error ocurred. There appears to be a misconfiguration in the form.",
+            // networkError: "The server cannot be reached, there is a problem with the network.",
+            // timeoutError: "The server cannot be reached, please try again.",
+            // serverError: "There was an error processing the password reset.",
+            // headerText: "Enter a new password for<br />{email}",
+            // title: "Change Password",
+            // weakPasswordError: "Password is too weak."
+            // passwordHistoryError: "Password has previously been used."
+        }
+    });*/
+})
